b2b537cc07a3adb26a138c26b6eb052864d3e147
farhanali-max <fa3993530@gmail.com>
2025-08-29 19:08:46 -0400

    Update CORS config and add manual changes for Vercel deployment

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const app = express();
 dotenv.config({ path: './config/config.env' });
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    methods: ['POST'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/message', messageRouter);
